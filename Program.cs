adf3a9d3dcb1c220c3daf47e9b7275314faaae4c
Cristian <cmsouzaac@outlook.com>
2023-01-25 21:46:49 -0300

    novo teste

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Guids
+{
+    class Program
+    {
+        static void Main()
+        {
+            var id = Guid.NewGuid();
+            id.ToString();
+
+            id = new Guid("ab59c2d1-a1b1-4399-89e4-eb9535f9516e");
+            //Console.WriteLine(id.ToString().Substring(0, 8));
+            Console.WriteLine(id);
+        }
+
+    }
+
+}
+
+/* 
+    Anotações:
+
+    Links para consultas:
+    https://learn.microsoft.com/pt-br/dotnet/api/system.guid?view=net-7.0 
+
+    - Guids para que um identificador seja usado exclusivamente, utilizamos o guids.
+    - Assim esse identificador possui a possibilidade muito baixa de duplicação.
+
+
+
+ */
