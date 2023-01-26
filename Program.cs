e14d35b87acbc15d3eb0f4b620bc6023438abfa8
Cristian <cmsouzaac@outlook.com>
2023-01-25 22:55:07 -0300

    alterações

--- Program.cs
+++ Program.cs
@@ -10,8 +10,11 @@ namespace Guids
             var id = Guid.NewGuid();
             id.ToString();
 
-            id = new Guid("ab59c2d1-a1b1-4399-89e4-eb9535f9516e");
+            //id = new Guid("ab59c2d1-a1b1-4399-89e4-eb9535f9516e");
+            id = new Guid();
             //Console.WriteLine(id.ToString().Substring(0, 8));
+
+            //if (id == Guid.NewGuid())
             Console.WriteLine(id);
         }
 
@@ -33,6 +36,7 @@ namespace Guids
         => Identificadores (id) possuem particularidades particulares.
         => Para realizar operações em BD utilizando esses id usamos guids.
         => Cada execução o compilador irá gerar um novo rach da guid.
+        => Passando para id somente um newGuid() sem paramentro retornará "0000"
     - Caso passassemos uma string para guid vazia o mesmo irá retornar
       erro de execeção.
     - Passando o mesmo rach para string o mesmo irá compilar o paramentro
